27ee747af703a893f8fb3e9fd1fd53063c4e65ce
NaveenThakur <naveencooljhinjer@gmail.com>
2020-07-15 08:26:32 +0530

    how to upload and retrieve Image With Firebase Database

---
+++ app/src/main/java/com/example/firbasewithimagess/ImageAdapter.java
@@ -0,0 +1,63 @@
+ package com.example.firbasewithimagess;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+
+public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHolder> {
+    private Context mContext;
+    private List<MyUpload> mUploads;
+
+    public ImageAdapter(Context context, List<MyUpload> uploads) {
+        mContext = context;
+        mUploads = uploads;
+    }
+
+    @Override
+    public ImageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(mContext).inflate(R.layout.image_item, parent, false);
+        return new ImageViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ImageViewHolder holder, int position) {
+        MyUpload uploadCurrent = mUploads.get(position);
+        holder.textViewName.setText(uploadCurrent.getName());
+        Picasso.with(mContext)
+                .load(uploadCurrent.getImageUrl())
+                .placeholder(R.mipmap.ic_launcher)
+                .fit()
+                .centerCrop()
+                .into(holder.imageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mUploads.size();
+    }
+
+    public class ImageViewHolder extends RecyclerView.ViewHolder {
+        public TextView textViewName;
+        public ImageView imageView;
+
+        public ImageViewHolder(View itemView) {
+            super(itemView);
+
+            textViewName = itemView.findViewById(R.id.text_view_name);
+            imageView = itemView.findViewById(R.id.image_view_upload);
+        }
+    }
+}
